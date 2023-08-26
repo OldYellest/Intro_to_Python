7574e177cd1628a97ea6b29bcd308cb3574269bb
oldyellest <oldyellest@skiff.com>
2023-08-26 16:05:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from library import greet
+
+def main():
+    greet()
+
+if __name__ == '__main__':
+    main()
+
+# Function tells IDE that this script is for execution and not for importing into other files
