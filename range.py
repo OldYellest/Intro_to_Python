dcaa7c033d5a20b177ca8c89ce0ac2d91be66ae0
oldyellest <oldyellest@skiff.com>
2023-08-27 22:30:21 +0200

    Range Lesson - v1.0

--- range.py
+++ range.py
@@ -8,6 +8,8 @@ def ex1range():
     tuple_a_range = tuple(a_range)
     print(tuple_a_range)
 
+# A set range between 2 numbers, or between 0 and X for a single number
+
 def ex2range():
     a_range = range(1, 10, 2)
 
@@ -20,3 +22,5 @@ def ex2range():
     tuple_a_range = tuple(a_range)
 
     print(tuple_a_range)
+
+# A range between 2 numbers, in intervals of the 3rd number
