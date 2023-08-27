d9bed5305db8e68e9be7102667a3d7627875ae67
oldyellest <oldyellest@skiff.com>
2023-08-27 22:30:27 +0200

    Index Lesson - v1.0

---
+++ index.py
@@ -0,0 +1,13 @@
+def exindex():
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', "H", "I"]
+
+    print(letters[0])
+    print(letters[1])
+    print(letters[2])
+
+    print(letters[-1])
+    print(letters[-2])
+
+# 0 starts at the first string, positive goes from 0 up
+# (-1) start at last string and goes back from there
+
