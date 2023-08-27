d602e255068827ea29952c097f63062463137443
oldyellest <oldyellest@skiff.com>
2023-08-27 21:35:50 +0200

    Example Files

---
+++ list.py
@@ -0,0 +1,12 @@
+def lists():
+    books = ["Moby Dick", "The Way of Men", "Extreme Ownership"]
+    random = ["Moby Dick", 1, 5.4, "The Way of Men"]
+
+    print(books)
+    print(books.pop())
+    print(books)
+    books.append('Concrete Hell')
+    print(books)
+
+#pop() returns the last value from the list and gets rid of it
+#XYZ.append() inserts a new item to the list
--- main.py
+++ main.py
@@ -1,14 +1,37 @@
+from inputs import inputs
+from list import lists
 from numbers import num
 from variables import var
-from inputs import inputs
+
+#from and imports only 'activate' once given function in script that refrences it
+#Otherwise, function causes error
 
 def main():
     print('Hello, World!!!')
 
-if __name__ == '__main__':
-    var()
+def programs():
+    program = ("inputs", 'list', 'main', 'numbers', 'variables')
+    print(program)
+    choice = input(f'Select a program from the list above: ')
 
-#main() runs Hello World
-#num() runs Math
-#var() runs Book
+    if choice == "inputs":
+        inputs()
+
+    if choice == "list":
+        lists()
+
+    if choice == "main":
+        main()
+
+    if choice == "numbers":
+        num()
+
+    if choice == "variables":
+        var()
+
+if __name__ == '__main__':
+    programs()
+
 # "if __name___" function tells IDE that this script is for execution and not for importing into other files
+
+
