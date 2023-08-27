4a99631f014e50d689bb678bfcfbe7791a76eeb5
oldyellest <oldyellest@skiff.com>
2023-08-27 20:05:03 +0200

    Global Command

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from numbers import num
+from variables import var
+from inputs import inputs
+
 def main():
-    print('Hello, World!!')
+    print('Hello, World!!!')
 
 if __name__ == '__main__':
-    main()
+    var()
 
-# Function tells IDE that this script is for execution and not for importing into other files
+#main() runs Hello World
+#num() runs Math
+#var() runs Book
+# "if __name___" function tells IDE that this script is for execution and not for importing into other files
