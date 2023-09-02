bfc94b728b8e7280995c414853003c5482b0635e
oldyellest <oldyellest@skiff.com>
2023-09-02 22:56:01 +0200

    len, min, max Examples v1.0

---
+++ length.py
@@ -0,0 +1,8 @@
+def length():
+    numbers = [9, 2, 0, 5, 1]
+
+    print(f'Numbers: {numbers}')
+    print(f'Length: {len(numbers)}')
+    print(f'Minimum: {min(numbers)}')
+    print(f'Maximum: {max(numbers)}')
+
