90ee9e1d54b129c998e10bd7af489e7b683f48ee
oldyellest <oldyellest@skiff.com>
2023-09-02 22:56:16 +0200

    plus, mult Examples v1.0

---
+++ plus.py
@@ -0,0 +1,12 @@
+def plus():
+    colors = ["Red", "Blue", "Yellow", "Green"]
+    more_colors = ["Cyan", "Magenta", "Orange"]
+
+    print(colors + more_colors)
+
+    # "+" appends the sequences together
+
+def mult():
+    colors = ["Red", "Blue", "Green"]
+
+    print(colors * 2)
