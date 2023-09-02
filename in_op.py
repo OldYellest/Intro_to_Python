dfe4f13ca24fac7fc5b4cb73e674d7a58db1ee60
oldyellest <oldyellest@skiff.com>
2023-09-02 23:16:40 +0200

    In Operation Examples v1.1

--- in_op.py
+++ in_op.py
@@ -5,12 +5,17 @@ def exin():
     a_string = 'Little Red Riding-Hood comes to me one Christmas Eve to give me information of the cruelty and ' \
                'treachery of that dissembling Wolf who ate her grandmother. '
 
-    print('Red' in a_string)
+    print(f'Books: {books}')
+    print(f'Movies: {movies}')
+    print(f'Numbers: {numbers}')
+    print(f'Story: {a_string}')
+
+    print(f"""Red is in the story: {'Red' in a_string}""")
 
     # Checks to see if it contains the word "red"
 
-    print('No Dentist' not in books)
-    print('RED' in movies)
-    print(16 not in numbers)
+    print(f"""'No Dentist' is not in the books list: {'No Dentist' not in books}""")
+    print(f"""'RED' is not in the movies list: {'RED' not in movies}""")
+    print(f"""16 is not in the numbers list: {16 not in numbers}""")
 
     # use of 'not' to find if string does not have object
