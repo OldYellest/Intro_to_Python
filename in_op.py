2e7df66dd750390afe783a331a1d60173f3cc359
oldyellest <oldyellest@skiff.com>
2023-09-02 22:55:41 +0200

    In Operations Examples v1.0

---
+++ in_op.py
@@ -0,0 +1,16 @@
+def exin():
+    books = ['Concrete Hell', 'No Doctor', 'Sniper Hide']
+    movies = ['RED', 'Cobweb', 'Dark Knight']
+    numbers = range(12)
+    a_string = 'Little Red Riding-Hood comes to me one Christmas Eve to give me information of the cruelty and ' \
+               'treachery of that dissembling Wolf who ate her grandmother. '
+
+    print('Red' in a_string)
+
+    # Checks to see if it contains the word "red"
+
+    print('No Dentist' not in books)
+    print('RED' in movies)
+    print(16 not in numbers)
+
+    # use of 'not' to find if string does not have object
